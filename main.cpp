c79b652efd9b6a43cebcf9153c48725481953b41
Sunjoo Park <all4dich@gmail.com>
2026-07-26 19:18:51 +0900

    First commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "TutorialConfig.h"
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, CMake!" << std::endl;
+    if ( argc < 2 ) {
+        std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        return 1;
+    }
+    return 0;
+}
